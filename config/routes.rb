f5b18eb429127572942a0c45bf84d619572f269e
Frederick Lazo <fredlazo@yahoo.com>
2016-07-11 23:53:07 -0700

    Checkpoint4 completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/show'
-
-  root 'welcome#index'
+  root 'users#show'
 
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
